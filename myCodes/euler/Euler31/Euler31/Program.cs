63d87cb8fa43dce35f5b49af5dbb6c86a26e786f
Victor Dulap <anunachi1337@gmail.com>
2020-09-13 11:27:04 +0300

    Euler 35, done!

--- myCodes/euler/Euler31/Euler31/Program.cs
+++ myCodes/euler/Euler31/Euler31/Program.cs
@@ -8,119 +8,46 @@ namespace Euler31
         {
             int totalWays = 4;
             int sum = 0;
-            //for (int _100 = 0; _100 < 1; _100++)
-            //{
-            //    sum += 100;
-            //    if (sum == 200)
-            //    {
-            //        totalWays++;
-            //        sum = 0;
-            //    }
 
-            //    for (int _50 = 0; _50 < 3; _50++)
-            //    {
-            //        sum += 50;
-            //        if (sum == 200)
-            //        {
-            //            totalWays++;
-            //            sum = 0;
-            //        } 
-
-            //        for (int _20 = 0; _20 < 9; _20++)
-            //        {
-            //            sum += 20;
-            //            if (sum == 200)
-            //            {
-            //                totalWays++;
-            //                sum = 0;
-            //            }
-
-            //            for (int _10 = 0; _10 < 19; _10++)
-            //            {
-            //                sum += 10;
-            //                if (sum == 200)
-            //                {
-            //                    totalWays++;
-            //                    sum = 0;
-            //                }
-
-            //                for (int _5 = 0; _5 < 39; _5++)
-            //                {
-
-            //                    sum += 5;
-            //                    if (sum == 200)
-            //                    {
-            //                        totalWays++;
-            //                        sum = 0;
-            //                    }
-
-            //                    for (int _2 = 0; _2 < 99; _2++)
-            //                    {
-
-            //                        sum += 2;
-            //                        if (sum == 200)
-            //                        {
-            //                            totalWays++;
-            //                            sum = 0;
-            //                        }
-
-            //                        for (int _1 = 0; _1 < 199; _1++)
-            //                        {
-            //                            sum += 1;
-            //                            if (sum == 200)
-            //                            {
-            //                                totalWays++;
-            //                                sum = 0;
-            //                            }
-            //                        }
-            //                    }
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-
-            //for (int _5 = 0; _5 < 1; _5++)
-            //{
-
-            //    sum += 5;
-            //    if (sum == 10)
-            //    {
-            //        totalWays++;
-            //        sum = 0;
-            //    }
-
-            //    for (int _2 = 0; _2 < 4; _2++)
-            //    {
-
-            //        sum += 2;
-            //        if (sum == 10)
-            //        {
-            //            totalWays++;
-            //            sum = 0;
-            //        }
-
-            //        for (int _1 = 0; _1 < 9; _1++)
-            //        {
-            //            sum += 1;
-            //            if (sum == 10)
-            //            {
-            //                totalWays++;
-            //                sum = 0;
-            //            }
-            //        }
-            //    }
-            //}
-
-            int max1 = 10, _1 = 0;
-            while (_1 != max1)
+            for (int _5 = 0; _5 < 1; _5++)
             {
-                if (sum + 1 < 10)
+                if ((sum + 5) >= 10)
                 {
-                    sum += 1;
+                    if ((sum + 5) == 10)
+                    {
+                        totalWays++;
+                        sum = 0;
+                    }
+                    else
+                    {
+                        //?
+                    }
                 }
-
-                _1++;
+                else { sum += 5; }
+                for (int _2 = 0; _2 < 4; _2++)
+                {
+                    if ((sum + 2) >= 10)
+                    {
+                        if ((sum + 2) == 10)
+                        {
+                            totalWays++;
+                            sum = 2;
+                        }
+                    }
+                    else { sum += 2; }
+                    for (int _1 = 0; _1 < 9; _1++)
+                    {
+                        if ((sum + 1) >= 10)
+                        {
+                            if ((sum + 1) == 10)
+                            {
+                                totalWays++;
+                                sum = 1;
+                            }
+                        }
+                        else { sum += 1; }
+                    }
+                }
             }
 
             Console.WriteLine("totalWays: " + totalWays);
