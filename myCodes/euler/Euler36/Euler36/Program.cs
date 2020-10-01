2355e6142603ced57a9a39d3b190103e07b170ab
Victor Dulap <anunachi1337@gmail.com>
2020-10-01 18:09:48 +0300

    added euler 36 and 37

---
+++ myCodes/euler/Euler36/Euler36/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Euler36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = Stopwatch.StartNew();
+
+            //int n;
+            //while (true)
+            //{
+            //    Console.Write("enter n: ");
+            //    n = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("base2: " + Convert.ToString(n, 2));
+            //    Console.WriteLine("base8: " + Convert.ToString(n, 8));
+            //    Console.WriteLine("base10: " + Convert.ToString(n, 10));
+            //    Console.WriteLine("base16: " + Convert.ToString(n, 16));
+            //}
+
+            //while (true)
+            //{
+            //    Console.Write("enter n: ");
+            //    int n = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine(Palindrome(n));
+            //}
+
+            int sum = 0;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                if (Palindrome(i) && Palindrome(Convert.ToString(i, 2)))
+                {
+                    sum += i;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("sum = " + sum);
+
+            clock.Stop();
+            Console.WriteLine($"Time: {clock.ElapsedMilliseconds} ms.");
+        }
+
+        static bool Palindrome(int nrToCheck)
+        {
+            string str = nrToCheck.ToString();
+
+            StringBuilder sb1 = new StringBuilder(/*str.Length / 2*/);
+            StringBuilder sb2 = new StringBuilder(/*str.Length / 2*/);
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                sb1.Append(str[i]);
+                sb2.Append(str[str.Length - i - 1]);
+            }
+
+            String str1 = new String(sb1.ToString());
+            String str2 = new String(sb2.ToString());
+
+            //Console.WriteLine("str1: " + str1);
+            //Console.WriteLine("str2: " + str2);
+
+            if (str1.Equals(str2))
+                return true;
+            else
+                return false;
+        }
+
+        static bool Palindrome(string nrToCheck)
+        {
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+            for (int i = 0; i < nrToCheck.Length / 2; i++)
+            {
+                sb1.Append(nrToCheck[i]);
+                sb2.Append(nrToCheck[nrToCheck.Length - i - 1]);
+            }
+
+            String str1 = new String(sb1.ToString());
+            String str2 = new String(sb2.ToString());
+
+            if (str1.Equals(str2))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ myCodes/euler/Euler37/Euler37/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Euler37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = Stopwatch.StartNew();
+
+            int count = 0; // max 11
+            int sum = 0;
+
+            int i = 8;
+            while (true)
+            {
+
+                if (i.ToString().Contains('0'))
+                {
+                    i++;
+                    continue;
+                }
+
+                if (Prime(i))
+                {
+                    int fakeILeftToRight = i;
+                    int fakeIRightToLeft = i;
+
+                    while (true)
+                    {
+                        if (((fakeILeftToRight.ToString().Length - 1) == 0) || ((fakeIRightToLeft.ToString().Length - 1) == 0))
+                        {
+                            if (Prime(fakeILeftToRight) && Prime(fakeIRightToLeft))
+                            {
+                                count++;
+                                sum += i;
+
+                                Console.WriteLine(i);
+                            }
+
+                            break;
+                        }
+                        else
+                        {
+                            fakeILeftToRight = TruncateLeftToRight(fakeILeftToRight);
+                            fakeIRightToLeft = TruncateRightToLeft(fakeIRightToLeft);
+
+
+                            if (!(Prime(fakeILeftToRight) && Prime(fakeIRightToLeft)))
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (count == 11) break;
+
+                i++;
+            }
+
+            Console.WriteLine("sum = " + sum);
+
+            clock.Stop();
+            Console.WriteLine($"Time {clock.ElapsedMilliseconds} ms.");
+        }
+
+        static bool Prime(int nr)
+        {
+            if (nr <= 1) return false;
+            else if (nr <= 3) return true;
+
+            for (int i = 2; i * i <= nr; i++)
+            {
+                if (nr % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static int TruncateLeftToRight(int nr)
+        {
+            List<char> nrs = new List<char>(nr.ToString());
+            if ((nrs.Count) > 1)
+                nrs.RemoveAt(0);
+
+            string str = new string(nrs.ToArray());
+
+            nr = Convert.ToInt32(str.ToString());
+
+            return nr;
+        }
+        static int TruncateRightToLeft(int nr)
+        {
+            List<char> nrs = new List<char>(nr.ToString());
+            if ((nrs.Count) > 1)
+                nrs.RemoveAt(nrs.Count - 1);
+
+
+            string str = new string(nrs.ToArray());
+
+            nr = Convert.ToInt32(str.ToString());
+
+            return nr;
+        }
+    }
+}
