903cb0bb794f79daf9ea29f46bbe499fdae2afcb
Victor Dulap <anunachi1337@gmail.com>
2020-12-01 16:16:27 +0200

    added euler46

--- myCodes/euler/Euler26/Euler26/Program.cs
+++ myCodes/euler/Euler26/Euler26/Program.cs
@@ -8,13 +8,54 @@ namespace Euler26
     {
         static void Main(string[] args)
         {
+            int maxRecurring = 0;
+            string maxRecurringNumber = "";
             for (int i = 2; i <= 10; i++)
             {
                 string str = ((decimal)1 / i).ToString();
                 str = str.Remove(0, 2);
+                str = str.Remove(str.Length - 1, 1);
 
-                Console.WriteLine(i + " | " + str);
+                if (str.Length > 10)
+                {
+                    
+                    bool moreToRemove = true;
+                    int j = 1;
+                    string recurringPart = "";
+                    string oldStrEnd = "";
+                    while(moreToRemove)
+                    {
+                        string strEnd = str.Substring(str.Length - j, j);
+                        string strRest = str.Remove(str.Length - j, j);
+
+                        if (strRest.Contains(strEnd))
+                        {
+                            j++;
+                            recurringPart = strEnd;
+                            oldStrEnd = recurringPart;
+                            if (recurringPart.Equals(oldStrEnd))
+                            {
+                                moreToRemove = false;
+                            }
+                        }
+                    }
+
+                    if (/*maxRecurring < j*/true)
+                    {
+                        maxRecurring = j;
+                        maxRecurringNumber = str;
+                    }
+                    Console.WriteLine(i + " | " + maxRecurringNumber + " | " + maxRecurring);
+
+                    //str = "123end";
+
+                    //Console.WriteLine(i + " | " + str);
+                    //Console.WriteLine("\tstrEnd = " + strEnd);
+                    //Console.WriteLine("\tstrRest = " + strRest);
+                }
             }
+
+            Console.WriteLine(maxRecurringNumber + " | " + maxRecurring);
         }
 
         static void Reccuring(string nr)
---
+++ myCodes/euler/Euler46/Euler46/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Euler46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = Stopwatch.StartNew();
+
+            int i = 9;
+            bool isOddComposite = true;
+
+            List<int> primes = new List<int>();
+
+            int lastPrimeN = 100000;
+            for (int j = 2; j < lastPrimeN; j++)
+            {
+                if (isPrime(j)) primes.Add(j);
+            }
+
+            while (isOddComposite)
+            {
+                //if(primes[primes.Count - 1] < i)
+                //{
+
+                //}
+
+                bool isCurrentComposite = false;
+                if(isPrime(i))
+                {
+                    i += 2;
+                    continue;
+                }
+
+                for (int j = 0; j <= primes.FindLastIndex(n => n < i); j++)
+                {
+                    //9 = 7 + 2×12
+                    int currentPrime = primes[j];
+                    for (int k = 0; k <= (i - currentPrime); k++)
+                    {
+                        if (i == (currentPrime + (2 * (k * k))))
+                        {
+                            //Console.WriteLine($"{i} = {currentPrime} + 2*{k}^2");
+                            isCurrentComposite = true;
+                            break;
+                        }
+                    }
+                    if (isCurrentComposite) break;
+                }
+
+
+                if (!isCurrentComposite)
+                {   
+                    isOddComposite = false;
+                    Console.WriteLine("Result: " + i);
+                }
+
+                i += 2;
+            }
+
+            clock.Stop();
+            Console.WriteLine($"Ellapsed time: {clock.ElapsedMilliseconds} ms.");
+        }
+
+        static bool isPrime(long nr)
+        {
+            if (nr <= 1) { return false; }
+            else if (nr == 2 || nr == 3) { return true; }
+
+            for (int i = 2; i * i <= nr; i++)
+            {
+                if (nr % i == 0) { return false; }
+            }
+
+            return true;
+        }
+    }
+}
